eb693dd9a6d04c76d2b22298bfc82a4826cf5fb7
Etay <etay.sade@xtrade.com>
2017-07-18 10:55:13 +0300

    HTTP added, working with hardcoded SC url

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import xs, {Stream} from "xstream"
 import {run} from '@cycle/run'
 import {makeDOMDriver,DOMSource, VNode, div} from '@cycle/dom'
+import {makeHTTPDriver, HTTPSource, RequestOptions} from "@cycle/http"
 import App from  './scripts/app'
 import Time from  './scripts/time'
 import { person } from "./scripts/person"
@@ -9,61 +10,93 @@ import Search from  './scripts/search'
 import Actions from  './scripts/actions'
 import Image from  './scripts/image'
 
+declare const SC: any
 
 namespace Sources {
     export interface dom {
         dom: DOMSource
+    }
+    export interface http {
+        http: HTTPSource
     }
 }
 namespace Sinks {
     export interface dom {
         dom: xs<VNode>
     }
+    export interface http {
+        http: xs<RequestOptions>
+    }
 }
 
-interface MainSources extends Sources.dom{}
+interface MainSources extends Sources.dom, Sources.http{}
 
 type MainSinks = {
        dom: xs<VNode>
+
 }
 
 function main (sources: MainSources): MainSinks {
 
     console.log(person.firstName + ' ' + person.lastName);
 
+
+    const request$ = sources.dom.select('.search-go').events('click')
+      .map(() => {
+        //   const setupURL = SC.get('/tracks',{
+        //               q: "britney"
+        //               , limit: 1// sources.limit,
+        //               , linked_partitioning: 1
+        //               , offset: false ? 0 + 4: 4
+        //           })
+        //           console.debug(setupURL._result)
+        return {
+          url: "https://api.soundcloud.com/tracks?q=asd&limit=6&linked_partitioning=1&offset=6&format=json&client_id=ggX0UomnLs0VmW7qZnCzw",
+          category: 'tracks',
+          method: 'GET'
+        };
+      });
+
+      const vtreeHttp$ = sources.http.select('tracks')
+        .flatten()
+        .map(res => res.body).debug('123')
+        .startWith(null)
+        .map(result =>
+          div([
+            //h2('.label', `Random number from server: ${result.number}`)
+            console.dir(result)
+        ])
+    )
+
     const app$  = App({dom: sources.dom})
         , timer$ = Time({dom: sources.dom})
-    //, dom$ = app.dom
-        // .mapTo(app.dom)
-        // .startWith(app.dom)
-        // .flatten()
-        //
         , search$ = Search({dom: sources.dom})
-
-
         , actions$ = Actions({dom: sources.dom})
         , image$ = Image({dom: sources.dom})
         //, recentSearch$ = RecentSearch({dom: sources.dom})
 
-        , dom$ = xs.combine(app$.dom, timer$.dom, search$.dom, actions$.dom, image$.dom)
-            .map(([appDom, timerDom, searchDom, actionsDom, imageDom]) => {
+        , dom$ = xs.combine(app$.dom, timer$.dom, search$.dom, actions$.dom, image$.dom, vtreeHttp$)
+            .map(([appDom, timerDom, searchDom, actionsDom, imageDom, vtreeHttp]) => {
                 return div(".main-holder", [
                       appDom
                     , timerDom
                     , searchDom
                     , actionsDom
                     , imageDom
+                    , vtreeHttp
                     ])
                 }
             )
         , sinks = {
-                 dom: dom$
+                   dom: dom$
+                 , http: request$
           }
     return sinks
 }
 
 const drivers = {
-  dom: makeDOMDriver('#app')
+    dom: makeDOMDriver('#app')
+  , http: makeHTTPDriver()
 }
 
 run(main, drivers)
--- src/scripts/search.ts
+++ src/scripts/search.ts
@@ -1,44 +1,19 @@
 import xs from "xstream"
 import {makeDOMDriver, DOMSource, VNode, div, input, button} from '@cycle/dom'
-
+/////import {makeHTTPDriver, HTTPSource, RequestOptions} from "@cycle/http"
 import { GetTracks } from  './sc'
 
 declare const SC: any
 
-
 interface SearchSources {
     dom: DOMSource
+    ///////http: HTTPSource
 }
 
 interface SearchSinks {
     dom: xs<VNode>
+////    http: xs<RequestOptions>
 }
-
-function getTracks(query: string) : VNode{
-
-     return SC.get('/tracks',{
-            q: query
-            , limit: 1// sources.limit,
-            , linked_partitioning: 1
-            , offset: false ? 0 + 4: 4
-        })
-        .then(function(tracks: any) {
-
-            //get next set only when clicked the next button
-            // if(sources.next){
-            //     //get the correct offset // seems a bit weird you need to do it like this or I missed something
-            //     offset = parseInt(getParameterByName('offset',tracks.next_href));
-            // }
-            //
-            console.log(tracks.collection[0].artwork_url)
-            //create list according to received tracks from SC
-            // $('.search-results ul').html('');
-            // $.each(tracks.collection, function(index, value) {
-            //     appendTrackDom('.search-results ul','<li>',value.artwork_url,value.title, value.id);
-            // })
-            return div(".bla", tracks.collection[0].artwork_url)
-        });
-    }
 
 function Search (sources: SearchSources): SearchSinks {
 
@@ -61,7 +36,7 @@ function Search (sources: SearchSources): SearchSinks {
                           "[Nothing yet]"
                         :
                           div(".search-results",[
-                                getTracks(clickedInput)
+                            //////////    getTracks(clickedInput, sources.http)
                               , div(".search-recent","[Recent searchs here]")
                           ])
 
@@ -69,7 +44,7 @@ function Search (sources: SearchSources): SearchSinks {
             })
 
     const vtree$ = xs.from(getTrack$)
-            .map((getTracks) => {
+            .map((drawSearch) => {
                 return div(".search_holder", [
                     div(".search-area",[
                           div('.search-holder',[
@@ -77,14 +52,15 @@ function Search (sources: SearchSources): SearchSinks {
                               , button('.search-go', "Go")
                           ])
                     ])
-                  , getTracks
+                  , drawSearch
                 ])
             })
 
 
 
     const sinks = {
-          dom: vtree$
+            dom: vtree$
+    //      , http: sources.http
     }
     return sinks
 }
