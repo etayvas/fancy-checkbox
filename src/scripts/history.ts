0cf69d9f0e454999bf1508014e9441461f9647e7
Etay <etay.sade@xtrade.com>
2017-08-01 16:31:35 +0300

    LS functionality

--- src/scripts/history.ts
+++ src/scripts/history.ts
@@ -1,45 +1,50 @@
 import xs from "xstream"
+import delay from 'xstream/extra/delay'
 import { Sources, Sinks } from '../scripts/definitions'
 import {makeDOMDriver, DOMSource, VNode, div, button} from '@cycle/dom'
 
 interface SHistorySources extends Sources.dom {}
 interface SHistorySinks extends Sinks.dom {}
 
+let   LSStatus = false
+    , prevInput = ""
 function SHistory (sources: SHistorySources): SHistorySinks {
-    const history = localStorage.getItem('history')
 
-    // const clickOnTrack$ = sources.dom.select(".track-list div").events("input")
-    //     .map(event => {
-    //         (event.target as HTMLInputElement).id
-    //         let history:string[] = [] ;
-    //         localStorage
-    //         ? [
-    //             localStorage.getItem('history') ? history = JSON.parse(localStorage.getItem('history') as string) : console.log('nothing in history (yet)')
-    //             , history.length === 5 ? [history.shift() , history = [...history, input] ]: history = [...history, input]
-    //             , localStorage.setItem('history', JSON.stringify(history))
-    //             , console.log(localStorage.getItem('history'))
-    //         ]
-    //         :   alert("can't use localStorage") //optimally needs to add fallbacks
-    //     })
+    const input$ = sources.dom.select(".search-input").events("input")
+        .map(event =>  (event.target as HTMLInputElement).value )
+        .startWith("")
+
+    , clickOnTrack$ = sources.dom.select(".track-list div").events("click")
+        .map(event =>  {
+            LSStatus = true
+            return true
+        }
+    )
+    .startWith(false)
+
+    , updateLS$ = xs.combine(clickOnTrack$, input$)
+        .map(([track, input]) => {
+
+            if(LSStatus && prevInput !== input && input.length > 2){
+
+                let history:string[] = [] ;
+                localStorage
+                ? [
+                    localStorage.getItem('history') && input !== "" ? history = JSON.parse(localStorage.getItem('history') as string) : ""
+                    , history.length === 5 ? [history.shift() , history = [...history, input] ]: history = [...history, input]
+                    , localStorage.setItem('history', JSON.stringify(history))
+                    , console.log(localStorage.getItem('history'))
+                ]
+                :   alert("can't use localStorage") //optimally needs to add fallbacks
+
+                LSStatus = false
+                prevInput = input
+            }
+        })
 
 
-
-    // const clickOnTrack$ = sources.dom.select(".track-list div").events("click")
-    //     .map(event => {
-    //         return (event.target as HTMLInputElement).id
-    //     })
-
-    // const requestSingleTrack$ = xs.combine(clickOnTrack$, actions.clickOnTrack$)
-    //     // filter if more than 2 chars and we have trackId
-    //     //.filter(([input, track]) => (input.length > 2 && track !== ""))
-    //     .map(([input, track]) => {
-    //         return {
-    //               url: `https://api.soundcloud.com/tracks/${track}?format=json&client_id=${cid}`
-    //             , category: "track"
-    //         }
-    //     })
-
-    const vtree$ = xs.of(
+    , history = localStorage.getItem('history')
+    , historyDOM$ = xs.of(
         div(".search-history",
             history
             ? (JSON.parse(history) as string[])
@@ -49,6 +54,11 @@ function SHistory (sources: SHistorySources): SHistorySinks {
             : div()
         )
     )
+
+    , vtree$ = xs.combine(historyDOM$, updateLS$)
+            .map(([v2, update]) => {
+                return v2
+                })
 
     const sinks = {
         dom: vtree$
--- src/scripts/search.ts
+++ src/scripts/search.ts
@@ -51,17 +51,6 @@ function Search (sources: SearchSources): SearchSinks {
         // filter if more than 2 chars
         .filter(([input, next]) => input.length > 2)
         .map(([input, next]) => {
-            //setup localStorage
-            let history:string[] = []
-            localStorage
-            ? [
-                localStorage.getItem('history') ? history = JSON.parse(localStorage.getItem('history') as string) : console.log('nothing in history (yet)')
-                , history.length === 5 ? [history.shift() , history = [...history, input] ]: history = [...history, input]
-                , localStorage.setItem('history', JSON.stringify(history))
-                , console.log(localStorage.getItem('history'))
-            ]
-            :   alert("can't use localStorage") //optimally needs to add fallbacks
-
         return {
               url: `https://api.soundcloud.com/tracks?format=json&client_id=${cid}&q=${input}&limit=6&linked_partitioning=1&offset=${next > 0 ? next : 0 }`
             , category: "list"
